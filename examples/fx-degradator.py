0e93c4f56ca64145ac4b36df90bc0599b5dd863c
Olivier Belanger <belangeo@gmail.com>
2016-09-14 23:36:00 -0400

    Modified degradator example.

--- examples/fx-degradator.py
+++ examples/fx-degradator.py
@@ -7,11 +7,6 @@ by analog inputs (use audio inputs after the stereo audio
 channels, ie. Input(2) is analog-in 0, Input(3) is 
 analog-in 1, etc.).
 
-It also show how to send signal to analog outputs. Again,
-use the outputs after the stereo audio channels, ie.
-.out(2) writes to analog-out 0, .out(3) to analog-out 1,
-etc.).
-
 """
 # Set to True if you want to control the degradation
 # and lowpass filter with analog inputs.
@@ -19,11 +14,6 @@ WITH_ANALOG_INPUT = True
 # If False, set frequency and brightness values.
 DEGRADATION = 0.75  # 0 -> 1
 LPCUTOFF = 2500	    # (Hz)
-
-# If True, a positive value is sent on analog-out 0 and 1
-# whenever there is an output signal (can be used to build 
-# a cheap vumeter with leds).
-WITH_ANALOG_OUTPUT = True
 
 # stereo input
 src = Input([0,1])
@@ -47,8 +37,4 @@ sr2 = invi1 * 0.49 + 0.01
 # DSP
 deg = Degrade(src, bit, [sr,sr2])
 lp = MoogLP(deg, freq, res=i1, mul=0.4).out()
-
-if WITH_ANALOG_OUTPUT:
-    # analog out 0-1 (stereo vumeter) 
-    fol = Sqrt(Clip(Follower(lp, mul=4))).out(2)
 
